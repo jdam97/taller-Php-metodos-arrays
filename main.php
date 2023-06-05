ea90a1e24f449d69651c71fc1e6f332fc82867ee
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-05 16:53:59 -0500

    feat: :sparkles: punto 7
    acabé el punto 7 que busca el nombre de la nave específico

--- main.php
+++ main.php
@@ -1,14 +1,19 @@
 <?php 
 /*
 *todo Taller métodos arrays
-*Punto 6
+*Punto 7
 */
+$naves = array(
+    "nave 1" => "Apolo 11",
+    "nave 2" => "Infinity",
+    "nave 3" => "Serenity",
+    "nave 4" => "Enterprise",
+    "nave 5" => "Prometheus"
+);
 
-$planetas = ["Mercury", "Venus", "Tierra", "Marte", "Jupiter", "Saturno","Urano","Neptuno"];
-
-if (in_array("Tierra",$planetas )){
-    echo "El planeta existe";
+if (in_array("Infinity",$naves )){
+    echo "La nave existe";
 }
-else {echo "No existe dentro del sistema solar"; };
+else {echo "No existe la nave"; };
 
 ?>
