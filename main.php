5a91263a486ef3a23a22a6b599b4856e6a18a6d6
JonathanDavidAlvarezMonsalveCAMPUS <jonathandalvarez7@gmail.com>
2023-06-05 16:02:12 -0500

    feat: :sparkles: punto 6
    eh acabado el punto 6 que calcula todas las masas de las naves

--- main.php
+++ main.php
@@ -1,26 +1,20 @@
 <?php 
 /*
 *todo Taller métodos arrays
-*Punto 4
-*debo hacer la funcion antes de usar el map
+*Punto 5
 */
 
-$gplanetas = array (
-    'Sol' => 1/3,
-    'Mercurio' => 1/4,
-    'Venus' => 2/3,
-    'Marte' => 4/8,
-    'Jupiter' => 7/10,
-    'Saturno' => 6/8,
-    'Urano' => 2/3,
-    'Neptuno' => 1/10
+$naves = array(
+    "apolo" => 2,
+    "La estrella de la muerte" => 10,
+    "sputnik" => 5,
+    "artemis" => 8
 );
 
-function calcular_gravedad($value,$key){
-    $gravedad_tierra = 9.8;
-    echo "El $key tiene una gravedad de: ". $value*$gravedad_tierra."<br>";
-
+function sumar ($naves){
+    $total = array_sum($naves);
+    return $total;
 };
-
-array_walk($gplanetas,"calcular_gravedad");
+$resultado = sumar($naves);
+print "Masa total de todas las naves: ".$resultado;
 ?>
